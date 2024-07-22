38891eee1ba73dfda82407d730a60ad0c770a6d0
Killers0992 <38152961+Killers0992@users.noreply.github.com>
2024-07-22 15:49:37 +0200

    Update

--- Packages/com.killers0992.avatarmanager-deuzear/Scripts/DeuzearMenu.cs
+++ Packages/com.killers0992.avatarmanager-deuzear/Scripts/DeuzearMenu.cs
@@ -1,12 +1,14 @@
 using UnityEditor;
+using UnityEngine;
 
 namespace AvatarManager.Core
 {
     public class DeuzearMenu : EditorWindow
     {
+#if AVATAR_MANAGER
         private BaseMenu _menuInstance;
 
-        public  BaseMenu MenuInstance
+        public BaseMenu MenuInstance
         {
             get
             {
@@ -22,24 +24,46 @@ namespace AvatarManager.Core
                 return _menuInstance;
             }
         }
+#endif
 
         [MenuItem("AvatarManager/Open Deuzear Menu")]
         public static void OpenWindow()
         {
-            GetWindow<DeuzearMenu>();
+            var window = GetWindow<DeuzearMenu>();
+
+#if !AVATAR_MANAGER
+            window.minSize = new Vector2(250f, 60f);
+            window.maxSize = window.minSize;
+#endif
         }
 
         public void OnGUI()
         {
+#if AVATAR_MANAGER
             if (MenuInstance.MenuAssetInstance == null)
                 MenuInstance.OnInitialize(this);
 
             MenuInstance.OnUpdate(this);
+#else
+            GUILayout.Label("Missing dependencies:");
+            GUILayout.Label(" - AvatarManager");
+            if (GUILayout.Button("Install"))
+            {
+                Install();
+            }
+#endif
+        }
+
+        void Install()
+        {
+            Application.OpenURL("https://avatarmanager.killers.dev/install");
         }
 
         public void CreateGUI()
         {
+#if AVATAR_MANAGER
             MenuInstance.OnCreateGUI(this);
+#endif
         }
     }
 }
